f7c8cb7f81c5a4d2fdf0ed2b8e0f73180db72dea
Aarush2108 <73521015+Aarush2108@users.noreply.github.com>
2021-01-30 11:54:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database,game,form,player;
+var gameState=0;
+var playerCount;
+var allPlayers;
+var distance =0;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(400,400);
+
+    game=new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    if(playerCount===4){
+        game.update(1);
+    }
+    if(gameState===1){
+        clear();
+        game.play();
+    }
+}
+
+
